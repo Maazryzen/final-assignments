00fc848030c5ce81befd87d243dd94e190076c2c
maaz khan <maazwarid289@gmail.com>
2024-06-03 03:19:08 -0700

    first commit

---
+++ AAgnment03/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous qoute and its author
+let qoute = "Nothings is impossible itself I M POSSIBLE";
+let author = "Maaz Khan";
+//printing the message
+console.log(`"${qoute}" - ${author}`);
---
+++ AAgnment03/main.ts
@@ -0,0 +1,10 @@
+
+// storing the famous qoute and its author
+
+let qoute: String= "Nothings is impossible itself I M POSSIBLE";
+
+let author: String= "Maaz Khan";
+
+//printing the message
+
+console.log(`"${qoute}" - ${author}`);
